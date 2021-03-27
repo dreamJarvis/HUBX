f4c86cf9f7ad77e23db20b2759de44a78c88b319
dreamJarvis <jhatarun16@gmail.com>
2021-03-27 19:53:42 +0530

    removed commented code

--- src/components/dashboard/Invite/InviteModal.jsx
+++ src/components/dashboard/Invite/InviteModal.jsx
@@ -4,13 +4,6 @@ import "materialize-css/dist/css/materialize.min.css";
 import TotalInvitePiechart from "./TotalInvitePiechart";
 
 class Modal extends Component {
-	state = {
-		totalNumberOfInvites: 50,
-		durationOfEvent : 50,
-		userName : '',
-		UserEmail: ''
-	}
-
 	componentDidMount() {
 		const options = {
 			inDuration: 250,
@@ -22,12 +15,6 @@ class Modal extends Component {
 		};
 		M.Modal.init(this.Modal, options);
 	};
-
-	setValues = (e)=>{
-		this.setState({
-			[e.target.id] : e.target.value
-		})
-	}
 
 	render() {
 		return (
@@ -43,32 +30,6 @@ class Modal extends Component {
 							<h5 className="row orange-text text-darken-2">Invite form</h5>
 						</div>
 						<div className="row right-align invite-content">
-							{/*<div className="col m6 invite-user-details">*/}
-							{/*	<div className="row">*/}
-							{/*		<div className="input-field col m12 s12 invite-userName">*/}
-							{/*			<div className="input-field col m12 s12">*/}
-							{/*				<label htmlFor="updatedUserName">Name</label>*/}
-							{/*				<input value={this.state.userName} id="userName" type="text" className="validate center-align" onChange={this.setValues}/>*/}
-							{/*			</div>*/}
-							{/*			<div className="input-field col m12 s12 invite-userEmail">*/}
-							{/*				<label htmlFor="updatedUserPhoneNumber">Email Id</label>*/}
-							{/*				<input value={this.state.UserEmail} id="UserEmail" type="email" className="validate center-align" onChange={this.setValues}/>*/}
-							{/*			</div>*/}
-							{/*		</div>*/}
-							{/*	</div>*/}
-							{/*	<div className="invite-stats">*/}
-							{/*		<div className="col s6 m6">*/}
-							{/*			<p className="grey-text">Total Invites : <span>{this.state.totalNumberOfInvites}</span></p>*/}
-							{/*		</div>*/}
-							{/*		<div className="col s6 m6">*/}
-							{/*			<p className="grey-text">Event Duration : <span>{this.state.durationOfEvent}</span></p>*/}
-							{/*		</div>*/}
-							{/*	</div>*/}
-							{/*</div>*/}
-							{/*<div className="col m6 invite-pie-chart center-align">*/}
-							{/*	<TotalInvitePiechart totalNumberOfInvites={this.state.totalNumberOfInvites} durationOfEvent={this.state.durationOfEvent}/>*/}
-								{/*<label className="price-calc">Total Invite Statistics</label>*/}
-							{/*</div>*/}
 							<TotalInvitePiechart/>
 						</div>
 					</div>
